423c416c6d37e67898c6da9dc1d58344fbab7141
Amir Kahriman <amirkahriman@Amirs-MacBook-Air.local>
2024-03-19 13:17:20 +0100

    Collected static files for production deployment

--- socialMedia/settings/base.py
+++ socialMedia/settings/base.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 MEDIA_URL = '/media/'
