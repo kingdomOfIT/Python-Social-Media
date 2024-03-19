1630e582364239542ef6eb18c194f34610e785ed
Amir Kahriman <amirkahriman@Amirs-MacBook-Air.local>
2024-03-19 14:18:04 +0100

    updated static files path

--- socialMedia/settings/base.py
+++ socialMedia/settings/base.py
@@ -100,10 +100,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static_root/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
