bf9a9b5f66a6b654ad77bdc5d037fa2c93225fa2
Amir Kahriman <amirkahriman@Amirs-MacBook-Air.local>
2024-03-20 07:10:14 +0100

    updated media root and url

--- socialMedia/urls.py
+++ socialMedia/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -17,7 +18,8 @@ urlpatterns = [
     path('follow/',include('follow.urls')),
     path('account/',include('allauth.urls')),
     path('api/schema', SpectacularAPIView.as_view(), name="schema"),
-    path('api/schema/docs', SpectacularSwaggerView.as_view(url_name="schema"))
+    path('api/schema/docs', SpectacularSwaggerView.as_view(url_name="schema")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
